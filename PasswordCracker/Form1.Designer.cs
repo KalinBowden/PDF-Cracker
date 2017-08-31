19c4840499b3f5f0adbf6b90011d2b7f78dd8b6c
Kalin Bowden <kalinbowden@gmail.com>
2017-08-31 18:03:31 -0500

    Added project files

---
+++ PasswordCracker/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace PasswordCracker
+{
+    partial class PasswordCracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.ForeColor = System.Drawing.Color.White;
+            this.lblTimer.Location = new System.Drawing.Point(89, 38);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 19);
+            this.lblTimer.TabIndex = 2;
+            // 
+            // PasswordCracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(221, 159);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "PasswordCracker";
+            this.Text = "Password Cracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ PasswordCracker/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordCracker
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class PasswordCracker : Form
+    {
+        // Class level variables
+        // TODO: Consider dividing the array into multiple arrays that can be selected and joined
+        char[] alphaNumeric = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'I', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+
+        // 
+        public PasswordCracker()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)//TODO: Fix Button event name
+        {
+            // Class level variables
+            char[] attempt = new char[100];
+            int[] hold = new int[1];
+
+            int count = 0;
+            string myString = "";
+            int i = 0;
+
+            // Cycle through all combonations of letters until the password is cracked.
+            while (string.Equals(textBox1.Text, GetAttempt(hold, attempt, myString)))//.Equals())
+            {
+                // Keep track of the number of attempts
+                count++;
+                MessageBox.Show(count + "\r\n" + textBox1.Text + "\r\n" + (myString = new string(attempt).Trim()));
+                //
+                if (hold[i] < 61)// If this is true, increment hold
+                {
+                    hold[i]++;
+                }
+                else if (hold[i] == 61) // If this is true, reset the current hold and move over to the next non 62 hold
+                {
+                    //
+                    do
+                    {
+                        //
+                        hold[i] = 0;
+                        i += 1;
+
+                        //
+                        if (hold.Length == i)
+                        {
+                            hold = SetNewArrayLength(hold);// Ima try it in debug. I am scratching my head atm
+                        }
+
+                        //
+                        hold[i]++;
+
+                    } while (hold[i] == 61);
+
+                    i = 0;
+                }
+                
+            }
+
+            //
+            MessageBox.Show("You did it!");
+            
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hold"></param>
+        public int[] SetNewArrayLength(int[] hold)
+        {
+            // Method level variables
+            int[] T = new int[hold.Length];
+            int i = 0;
+
+            //
+            hold.CopyTo(T, 0);
+            hold = new int[hold.Length + 1];
+
+            //
+            foreach (int num in T)
+            {
+                hold[i++] = num;
+            }
+
+            //
+            hold[hold.Length - 1] = 0;
+
+            return hold;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hold"></param>
+        /// <param name="attempt"></param>
+        /// <returns></returns>
+        public string GetAttempt(int[] hold, char[] attempt, string myString)
+        {
+            //
+
+
+            //
+            for (int i = 0; i < hold.Length; i++)
+            {
+                attempt[i] = alphaNumeric[hold[i]];
+            } 
+
+            return myString = new string(attempt).Trim();
+        }
+
+    }
+}
